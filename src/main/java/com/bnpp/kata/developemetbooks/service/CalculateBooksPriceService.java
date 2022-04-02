05aa5226d810b7ecbe9d7ce45789c9ff9890e5f2
2022-DEV1-018 <2022.dev1.018@gmail.com>
2022-04-02 10:08:20 +0530

    Refactored the code for better readability

--- src/main/java/com/bnpp/kata/developemetbooks/service/CalculateBooksPriceService.java
+++ src/main/java/com/bnpp/kata/developemetbooks/service/CalculateBooksPriceService.java
@@ -8,33 +8,13 @@ import com.bnpp.kata.developemetbooks.model.DiscountEnum;
 
 public class CalculateBooksPriceService {
 
+	private static final int BOOK_PRICE = 50;
 	private static final double TOTAL_PERCENTAGE = 100.0;
-	private static final int FIRST_INDEX = 0;
 
 	public double calculateBooksPrice(List<BookApiRequest> bookApiRequestList) {
-		double calculatedPrice = 0d;
-		double price = 50d;
-		if (bookApiRequestList.size() == 1) {
-			BookApiRequest bookApiRequest = bookApiRequestList.get(FIRST_INDEX);
-			calculatedPrice = 50d * bookApiRequest.getQuantity();
-		} else if (bookApiRequestList.size() == 2) {
-			double basePrice = bookApiRequestList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 5) / TOTAL_PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 3) {
-			double basePrice = bookApiRequestList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 10) / TOTAL_PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 4) {
-			double basePrice = bookApiRequestList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 20) / TOTAL_PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		} else if (bookApiRequestList.size() == 5) {
-			double basePrice = bookApiRequestList.size() * price;
-			double discountedPriceForSet = basePrice - ((basePrice * 25) / TOTAL_PERCENTAGE);
-			calculatedPrice += discountedPriceForSet;
-		}
-		return calculatedPrice;
+		List<Integer> inputBookIds = getBookIdList(bookApiRequestList);
+		List<List<Integer>> defaultCombinationSets = getDefaultBooksCombination(inputBookIds);
+		return getBooksPrice(defaultCombinationSets);
 	}
 
 	protected List<Integer> getBookIdList(List<BookApiRequest> bookRequestList) {
@@ -95,8 +75,8 @@ public class CalculateBooksPriceService {
 
 	private double calculateDiscountedPrice(List<Integer> set, double discountForSet) {
 		double calculatedPrice = 0d;
-		double basePrice = set.size() * 50;
-		double discountedPriceForSet = basePrice - ((basePrice * discountForSet) / 100.0);
+		double basePrice = set.size() * BOOK_PRICE;
+		double discountedPriceForSet = basePrice - ((basePrice * discountForSet) / TOTAL_PERCENTAGE);
 		calculatedPrice += discountedPriceForSet;
 		return calculatedPrice;
 	}
